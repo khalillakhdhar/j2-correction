0a2284d91a838d3b5115889717c693e80a6f0c12
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-21 09:33:59 +0100

    Update Testes.php

--- Testes.php
+++ Testes.php
@@ -1,6 +1,16 @@
 <?php 
+require("validation.php");
 class Testes extends \PHPUnit\Framework\TestCase
 {
+    public $v;
+/** @before */
+public function init()
+{
+    $this->v=new Validation();
+
+}
+
+
 /** @test */
 function testNom()
 {
@@ -18,6 +28,35 @@ public function testNote()
 {
     $note=17;
     $this->assertGreaterThan(0,$note);
+}
+/** @test */
+public function testDiv()
+{
+    
+    $b=4;
+    $this->assertTrue($this->v->verifNotNull($b));
+}
+/** @test */
+public function TestSoustraction()
+{
+$a=12;
+$b=10;
+$this->assertTrue($this->v->verifSoustraction($a,$b));
+
+}
+/** @test */
+public function notEgale()
+{
+    $x=5;
+    $y=4;
+    $this->assertTrue($this->v->verifDifference($x,$y));
+
+}
+/** @test */
+public function testParite()
+{
+    $p=20;
+    $this->assertTrue($this->v->verifParite($p),"le paramétre est impaire!");
 }
 }
 
